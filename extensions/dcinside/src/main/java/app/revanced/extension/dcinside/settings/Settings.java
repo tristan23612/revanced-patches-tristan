7a0d3f269dbf13d1f0c525f92c17bf2857fb4a64
Tristan <tristan23612@gmail.com>
2026-08-08 00:26:36 +0900

    feat(dcinside - HideBottomLikePosts): add setting to hide recommended posts below next/previous post

--- extensions/dcinside/src/main/java/app/revanced/extension/dcinside/settings/Settings.java
+++ extensions/dcinside/src/main/java/app/revanced/extension/dcinside/settings/Settings.java
@@ -13,17 +13,21 @@ import app.revanced.extension.shared.settings.BooleanSetting;
  * menu icons, search history, etc.) are loaded together with this class.
  */
 public class Settings extends BaseSettings {
+    // ADS
     public static final BooleanSetting HIDE_ADS = new BooleanSetting("revanced_hide_ads", TRUE, TRUE);
-    public static final BooleanSetting DISABLE_POST_PUM_OPTION = new BooleanSetting("revanced_disable_post_pum_option", TRUE, FALSE);
+    // FEED
     public static final BooleanSetting HIDE_DCBEST = new BooleanSetting("revanced_hide_dcbest", TRUE, TRUE);
     public static final BooleanSetting HIDE_RECOMMENDED_GALLERIES = new BooleanSetting("revanced_hide_recommended_galleries", TRUE, TRUE);
     public static final BooleanSetting HIDE_CROWD = new BooleanSetting("revanced_hide_crowd", TRUE, TRUE);
     public static final BooleanSetting HIDE_NEW_GALLERIES = new BooleanSetting("revanced_hide_new_galleries", TRUE, TRUE);
     public static final BooleanSetting HIDE_RECENT = new BooleanSetting("revanced_hide_recent", TRUE, TRUE);
+    // GENERAL
+    public static final BooleanSetting DISABLE_POST_PUM_OPTION = new BooleanSetting("revanced_disable_post_pum_option", TRUE, FALSE);
     public static final BooleanSetting SHOW_USER_ID = new BooleanSetting("revanced_show_user_id", TRUE, FALSE);
+    // MISC
     public static final BooleanSetting ENABLE_QUICK_POST_MANAGEMENT = new BooleanSetting("revanced_enable_quick_post_management", FALSE, FALSE);
     public static final BooleanSetting HIDE_ADMINISTRATOR_NOTICE = new BooleanSetting("revanced_hide_administrator_notice", TRUE, FALSE);
     public static final BooleanSetting HIDE_MUST_READ_NOTICE = new BooleanSetting("revanced_hide_must_read_notice", TRUE, FALSE);
     public static final BooleanSetting DISABLE_UPDATE_CHECK = new BooleanSetting("revanced_disable_update_check", FALSE, FALSE);
-    public static final BooleanSetting HIDE_BOTTOM_LIKE_POSTS = new BooleanSetting("revanced_hide_bottom_like_posts", FALSE, FALSE);
+    public static final BooleanSetting HIDE_BOTTOM_LIKE_POSTS = new BooleanSetting("revanced_hide_bottom_like_posts", TRUE, FALSE);
 }
