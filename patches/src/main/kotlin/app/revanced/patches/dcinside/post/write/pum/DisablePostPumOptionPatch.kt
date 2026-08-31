db9fbc54ed2825ecd858cdb7384d7b3633a6e0cf
Tristan <tristan23612@gmail.com>
2026-09-01 00:57:10 +0900

    feat(dcinside - DisablePostPumOptionPatch): update method and instruction usage for compatibility with v5.3.4

--- patches/src/main/kotlin/app/revanced/patches/dcinside/post/write/pum/DisablePostPumOptionPatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/dcinside/post/write/pum/DisablePostPumOptionPatch.kt
@@ -1,7 +1,6 @@
 package app.revanced.patches.dcinside.post.write.pum
 
 import app.revanced.patcher.extensions.addInstructions
-import app.revanced.patcher.extensions.fieldReference
 import app.revanced.patcher.extensions.getInstruction
 import app.revanced.patcher.patch.bytecodePatch
 import app.revanced.patches.all.misc.resources.addResources
@@ -10,7 +9,7 @@ import app.revanced.patches.dcinside.misc.extension.sharedExtensionPatch
 import app.revanced.patches.dcinside.misc.settings.PreferenceScreen
 import app.revanced.patches.dcinside.misc.settings.settingsPatch
 import app.revanced.patches.shared.misc.settings.preference.SwitchPreference
-import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 
 private const val EXTENSION_CLASS_DESCRIPTOR = "Lapp/revanced/extension/dcinside/patches/post/write/pum/DisablePostPumOptionPatch;"
 
@@ -21,7 +20,7 @@ val disablePostPumOptionPatch = bytecodePatch(
 ) {
     compatibleWith(
         "com.dcinside.app.android"(
-            "5.3.2"
+            "5.3.4"
         )
     )
 
@@ -38,24 +37,19 @@ val disablePostPumOptionPatch = bytecodePatch(
             SwitchPreference("revanced_disable_post_pum_option"),
         )
 
-        var postWriteActivityDefiningClass = ""
-        var notAllowedPumEnableFieldReference = ""
-        postWriteActivityHelperMethodMatch.let {
+        writeConfigNotAllowedPumSharedPreferenceMethodMatch.let {
             it.method.apply {
-                postWriteActivityDefiningClass = definingClass
+                val defaultBooleanIndex = it[0]
+                val defaultBooleanRegister = getInstruction<OneRegisterInstruction>(defaultBooleanIndex).registerA
 
-                val notAllowedPumEnableFieldIndex = it[2]
-                notAllowedPumEnableFieldReference = getInstruction<TwoRegisterInstruction>(notAllowedPumEnableFieldIndex).fieldReference!!.toString()
+                addInstructions(
+                    defaultBooleanIndex + 1,
+                    $$"""
+                        invoke-static { }, $$EXTENSION_CLASS_DESCRIPTOR->shouldDisablePostPumOption()Z
+                        move-result v$${defaultBooleanRegister}
+                    """
+                )
             }
         }
-
-        postWriteActivityInitMethodMatch(postWriteActivityDefiningClass).method.addInstructions(
-            0,
-            """
-                invoke-static { }, $EXTENSION_CLASS_DESCRIPTOR->shouldDisablePostPumOption()Z
-                move-result v0
-                iput-boolean v0, p0, $notAllowedPumEnableFieldReference
-            """
-        )
     }
 }
